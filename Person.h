d8b31e5a453fb35ff4796d76db531ac4902381b9
Pulitzer莉莉 <1217407041@qq.com>
2018-05-25 02:10:11 +0800

    no message

---
+++ Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  UISomethingTest
+//
+//  Created by yaoxianer on 2018/5/15.
+//  Copyright © 2018年 坐吃等死. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *strCopy;
+@property (nonatomic, strong) NSString *strStrong;
+
+@end
---
+++ Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  UISomethingTest
+//
+//  Created by yaoxianer on 2018/5/15.
+//  Copyright © 2018年 坐吃等死. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+
+
+@end
